e53c360f411a9bafd21b5ac88abf6253f70635dd
Nicole <144697436+Nicole0509@users.noreply.github.com>
2024-07-09 03:11:19 +0200

    Update CartPage.jsx

--- src/CartPage.jsx
+++ src/CartPage.jsx
@@ -53,14 +53,14 @@ const SingleCartProduct = ({ cart, idx }) => {
   const [qty, setQty] = useState(cart.quantity)
   useEffect(() => { updateProductQuantityInCart(idx, qty) }, [qty])
   return (
-    <div className="ListComponent">
-      <div> <div ><img className='ImageListComponent' src={cart.imageUrl} /></div></div>
-      <div className="ListDescriptionComponent">
-        <span>{cart.category}</span>
-        <span>{cart.pieces}</span>
-        <span>{cart.colors}</span>
+    <div className="w-full flex justify-between p-15 my-4 ">
+      <div className="flex justify-center items-center w-32 py-2 px-5 border-2 rounded-lg"> <div className="flex rounded-lg justify-center items-center overflow-hidden"><img className='w-24 h-fit object-cover' src={cart.imageUrl} /></div></div>
+      <div className="flex flex-col">
+        <span className ="ListDescriptionComponent">{cart.category}</span>
+        <span className ="ListDescriptionComponent">{cart.pieces}</span>
+{/*         <span>{cart.colors}</span> */}
       </div>
-      <div className="ListQuantity">
+      <div className=" w-32 flex items-center gap-5">
         <button onClick={() => qty > 1 && setQty(qty - 1)}>-</button>
         <span>{cart.quantity}</span>
         <button onClick={() => setQty(qty + 1)}>+</button>
